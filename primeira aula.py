c4e2ac5122b1fcf1c08073148149f81a0a3a0d91
Michelle de Souza <michelle.ubuntulh@gmail.com>
2020-09-21 01:23:23 -0300

    primeiros codigos python

---
+++ primeira aula.py
@@ -0,0 +1,15 @@
+print("meu primeiro codido de python")
+
+A= 5
+B= 2
+soma= A+B
+
+print(soma)
+
+a=5
+print(a)
+b=2
+a=b-b
+print(a)
+a=b-b
+print('a')
---
+++ segunda aula.py
@@ -0,0 +1,38 @@
+'''soma = 5 + 5
+print('soma: {}'.format(soma))
+'''
+
+# a= int(input(' Entre com o primeiro valor '))
+# b= int(input('Entre com o segundo valor '))
+# print (type (a))
+# soma= a+b
+# subtracao= a-b
+# multiplicacao= a*b
+# divisao= a/b
+# resto= a%b
+# # print(soma)
+# # print(subtracao)
+# # print(multiplicacao)
+# # print(divisao)
+# # print(resto)
+# # print('soma: ' + str(soma))
+# # #ou pode ser
+# # print('soma: {} ' .  format(soma))
+# resultado= ('Soma:  {soma}. \nSubtração: {subtracao} '
+#       '\nMultiplicação: {multiplicacao}'
+#       '\nDivisão: {divisao}'
+#       '\nResto: {resto}' .format(soma=soma ,
+#                                  subtracao=subtracao,
+#                                  resto=resto,
+#                                  divisao=divisao,
+#                                  multiplicacao=multiplicacao))
+
+print(resultado )
+# x='1'
+# soma2 = int(x) + 1
+# print(soma2)
+
+
+
+
+
